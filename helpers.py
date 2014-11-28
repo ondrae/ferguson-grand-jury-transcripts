4dd5596dbbfc8b616d13fe1e7f7c6254335f716e
Andrew Hyder <hyd415@gmail.com>
2014-11-27 23:34:49 -0800

    Build an Akoma Ntoso file

---
+++ helpers.py
@@ -0,0 +1,15 @@
+def indent(elem, level=0):
+  ''' pretty printing for xml '''
+  i = "\n" + level*"  "
+  if len(elem):
+      if not elem.text or not elem.text.strip():
+          elem.text = i + "  "
+      if not elem.tail or not elem.tail.strip():
+          elem.tail = i
+      for elem in elem:
+          indent(elem, level+1)
+      if not elem.tail or not elem.tail.strip():
+          elem.tail = i
+  else:
+      if level and (not elem.tail or not elem.tail.strip()):
+          elem.tail = i
